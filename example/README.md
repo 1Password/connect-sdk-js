1dd2d8f2fdb0738c82a315083122d6b082221034
Horia Culea <horia.culea@agilebits.com>
2021-05-11 13:41:00 +0300

    Add containerized example

---
+++ example/README.md
@@ -0,0 +1,31 @@
+# JavaScript Connect SDK Example
+
+This example has been created in order to illustrate the main available functionality with regard to item manipulation, in the JavaScript Connect SDK.
+Given a (secret) string, this example will create an item, add it to given vault, retrieve it and, eventually, remove it.
+
+## Prerequisites
+
+In order to be able to run this example, one will need:
+
+* [Docker](https://docs.docker.com/install/), installed and running
+* an [1Password Connect](https://support.1password.com/secrets-automation/#step-2-deploy-a-1password-connect-server) server, hosted on your infrastructure
+* a Connect token, with read/write permissions for at least one vault accessible by the Connect instance
+
+
+## Running the example
+
+Build the JavaScript docker demo
+```
+ docker build -t js-connect-sdk-example .
+```
+
+Run the docker demo with the required fields passed as environment variables
+```
+docker run -it -e OP_CONNECT_TOKEN=<YOUR_CONNECT_TOKEN> -e OP_VAULT=<YOUR_VAULT_ID> /
+    -e OP_CONNECT_HOST=<YOUR_CONNECT_HOST> -e SECRET_STRING=<ANY_RANDOM_STRING> /
+    js-connect-sdk-example
+```
+
+If your connect instance is deployed locally, the `1PASSWORD_CONNECT_HOST` environment variable should be set to `http://host.docker.internal:8080`.
+
+You should now be able to see, in real time, the 5 different steps (creation of client, creation of item, posting to vault, retrieval from vault, deletion) as they happen.
---
+++ example/index.js
@@ -0,0 +1,63 @@
+import { OnePasswordConnect, ItemBuilder } from "@1password/connect";
+import * as readline from "readline-sync"
+import steps from steps
+
+const steps = {
+    "intro": "\nHello from 1Password! In order to exemplify the end-to-end process of creating, posting, retrieving and, eventually, deleting an item, the following steps are taken: \n",
+    "step1": '1. The SDK has contacted the Connect Server, and a client has been created, based on the provided OP_CONNECT_TOKEN.',
+    "step2": '2. An item containing the secret string has been successfully created.',
+    "step3": '3. The item containing the secret string has been successfully added in the default vault.',
+    "step4": '4. The item containing the secret string has been successfully retrieved from the default vault.\n',
+    "confirmation": 'Would you like to delete the newly created item from your vault? (y/n)',
+    "confirmation2": "Your answer should be either 'y' or 'n'. Would you like to delete the newly created item from your vault? (y/n)",
+    "step5": '\n5. The item containing the secret string has been successfully deleted from the default vault.\n',
+    "outro": 'All done!'
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+console.log(steps["intro"])
+
+// CREATE CLIENT
+const op = OnePasswordConnect({
+	serverURL: process.env.OP_CONNECT_HOST,
+	token: process.env.OP_CONNECT_TOKEN,
+	keepAlive: true,
+});
+console.log(steps["step1"])
+
+// CREATE ITEM
+const newItem = new ItemBuilder()
+	.addField({
+		value: process.env.SECRET_STRING,
+		type: "STRING",
+	}).setCategory("LOGIN")
+	.build();
+console.log(steps["step2"])
+
+// ADD ITEM TO VAULT
+const createdItem = await op.createItem(process.env.OP_VAULT, newItem);
+console.log(steps["step3"])
+
+await sleep(10000)
+
+// RETRIEVE ITEM FROM VAULT
+const retrievedItem = await op.getItem(process.env.OP_VAULT, createdItem.id);
+console.log(steps["step4"])
+
+var answer = readline.question(steps["confirmation"]);
+
+while (answer!='y' && answer!='n') {
+    answer = readline.question(steps["confirmation2"]);
+}
+
+if (answer == 'y') {
+    // DELETE ITEM FROM VAULT
+    await op.deleteItem(process.env.OP_VAULT, retrievedItem.id);
+    console.log(steps["step5"])
+}
+
+console.log(steps["outro"])
+
