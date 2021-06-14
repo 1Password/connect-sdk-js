0caa67f0915cbc203942b5d1455763fd369553e0
Eddy Filip <eddy.filip@agilebits.com>
2021-06-14 23:33:57 +0300

    Make the coverage report in a format recognized by codeconv

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ["./src/", "./__test__"],
-  collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx,ts}"],
   coverageDirectory: "./coverage/",
-  coverageReporters: ["json-summary"]
 };
