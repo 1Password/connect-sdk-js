680471eb4b701b561efe389d4426f3517e1ae1b6
volodymyrZotov <volodymyr.zotov@gmail.com>
2022-07-22 10:37:24 +0300

    fixed typo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const ERROR_MESSAGE = {
     NO_VAULTS_FOUND_BY_TITLE: "No Vaults found with title",
-    MULTIPLE_VAULTS_FOUND_BY_TITLE: "Found multiple Vaults with given title. Provide a more specific Item title",
+    MULTIPLE_VAULTS_FOUND_BY_TITLE: "Found multiple Vaults with given title. Provide a more specific Vault title",
 
     NO_ITEMS_FOUND_BY_TITLE: "No Items found with title",
     MULTIPLE_ITEMS_FOUND_BY_TITLE: "Found multiple Items with given title. Provide a more specific Item title",
