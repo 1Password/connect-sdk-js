7d7b495695c78a459586d9e953610d07bc7ee699
ephrem1 <ephrem@addissoftware.com>
2022-08-25 10:21:57 +0300

    feat: add search by title which contains given string

--- src/lib/utils/query-builder.ts
+++ src/lib/utils/query-builder.ts
@@ -8,9 +8,13 @@ export const FILTER_PARAM = {
 
 const eqByTitle = (title: string): string => `${FILTER_PARAM.TITLE} eq "${title}"`;
 
+const coByTitle = (title: string): string => `${FILTER_PARAM.TITLE} co "${title}"`;
+
 const buildKeyValuePair = (queryParamName: string): (queryParamValue: string) => string =>
     (queryParamValue): string => `${queryParamName}=${queryParamValue}`;
 
 const buildFilterQuery = buildKeyValuePair(QUERY_PARAM_NAME.FILTER);
 
 export const filterByTitle = (title: string): string => buildFilterQuery(eqByTitle(title));
+
+export const searchByTitle = (title: string): string => buildFilterQuery(coByTitle(title));
